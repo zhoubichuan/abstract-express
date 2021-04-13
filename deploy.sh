a4a951843b1821fdf737964365b21cb48cb9b034
zhoubichuan <zhoubichuan@icloud.com>
2021-04-13 12:53:14 +0800

    master

--- deploy.sh
+++ deploy.sh
@@ -11,19 +11,19 @@ git pull origin master
 git push -u origin master
 git push origin master
 
-# 2.打包
-npm run src:build
+# # 2.打包
+# npm run src:build
 
-# 3.进入打包目录
-cd dist
+# # 3.进入打包目录
+# cd dist
 
-# 4.发布到gh-pages分支
-git init
-git checkout --orphan gh-pages
-git add .
-git commit -m 'gh-pages'
-git remote add origin https://github.com/zhoubichuan/abstract-express.git
-git push -f origin gh-pages
+# # 4.发布到gh-pages分支
+# git init
+# git checkout --orphan gh-pages
+# git add .
+# git commit -m 'gh-pages'
+# git remote add origin https://github.com/zhoubichuan/abstract-express.git
+# git push -f origin gh-pages
 
-# 5.返回初始目录
-cd -
+# # 5.返回初始目录
+# cd -
